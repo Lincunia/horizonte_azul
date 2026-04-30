385571f7b52fb33a96a4a749ad1acc6127ee6f71
cnajerat-hash <cnajerat@ucentral.edu.co>
2026-04-30 12:25:43 -0500

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
 	<div>
+		<button @click="incrementar">Contador: {{ count }}</button>
+	</div>
+	<div>		
 		<router-view />
 	</div>
 </template>
+
+  
+  <script setup lang="ts">
+  import { ref } from 'vue'
+  
+  const count = ref<number>(0)
+  
+  function incrementar(): void {
+	count.value++
+  }
+  </script>
